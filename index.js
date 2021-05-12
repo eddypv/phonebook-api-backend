2463fe658b2ff494b614a30c3db3b23bf94635ee
Eddy <eddypv0@gmail.com>
2021-05-11 22:20:03 -0400

    Add ESLint

--- index.js
+++ index.js
@@ -1,88 +1,70 @@
-const { json, response } = require('express')
+const { json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(json())
-morgan.token('Body', (req, res)=> {
-    const body = req.body
-    if(Object.keys(body).length > 0 ) 
-        return JSON.stringify(body)
-    else 
-        return ""
+morgan.token('Body', (req, res) => {
+  const body = req.body
+  if (Object.keys(body).length > 0) { return JSON.stringify(body) } else { return '' }
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :Body'))
 
 let persons = [
-    {
-    "name": "Arto Hellas",
-    "number": "123123",
-    "id": 1
-    },
-    {
-    "name": "Dan Abramov",
-    "number": "123123",
-    "id": 2
-    },
-    {
-    "name": "Juan Pablo",
-    "number": "1234664",
-    "id": 3
-    }
+  {
+    name: 'Arto Hellas',
+    number: '123123',
+    id: 1
+  },
+  {
+    name: 'Dan Abramov',
+    number: '123123',
+    id: 2
+  },
+  {
+    name: 'Juan Pablo',
+    number: '1234664',
+    id: 3
+  }
 ]
 
-app.get("/api/persons/info", (request, response)=>{
-    const info = `<p>Phonebook has info for ${persons.length} persons </p>
+app.get('/api/persons/info', (request, response) => {
+  const info = `<p>Phonebook has info for ${persons.length} persons </p>
     <p>${new Date()}</p>`
-    response.send(info)
+  response.send(info)
 })
-app.get("/api/persons", (request, response)=>{
-
-    response.json(persons)
-})
-
-app.get("/api/persons/:id", (request, response)=>{
-    const id = Number(request.params.id)
-    const person = persons.find(item => item.id === id )
-    if(typeof person === 'undefined' )
-        response.status(404).end()
-    else 
-        response.json(person)
-
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.delete("/api/persons/:id", (request, response)=>{
-    const id = Number(request.params.id)
-    persons = persons.filter(item => item.id !== id )
-    response.status(204).end()
-
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(item => item.id === id)
+  if (typeof person === 'undefined') { response.status(404).end() } else { response.json(person) }
 })
 
-app.post("/api/persons", (request, response)=>{
-    const newPerson = request.body
-    let messageError = ""
-    if (typeof newPerson.name === "undefined" || typeof newPerson.number === "undefined")
-        messageError = "The name or number is missing"
-    else if(newPerson.name.trim() === "")
-        messageError = "name is required"
-    else if ( newPerson.number.trim() === "")
-        messageError = "number is required"
-    else{
-        const existsPerson = persons.find(item => item.name === newPerson.name.trim() )
-        if(typeof existsPerson !== "undefined") 
-            messageError = "name must be unique"
-    }
-
-    if(messageError === ""){
-        const ids = persons.map(item => item.id)
-        const maxId = Math.max(...ids)
-        newPerson.id = maxId +1
-        persons = persons.concat(newPerson)
-        response.status(201).json(newPerson)
-    }else 
-        response.status(400).json({"error":messageError})
-
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(item => item.id !== id)
+  response.status(204).end()
 })
-const PORT=3001
-app.listen(PORT, ()=>{
-    console.log(`listennig by http://localhost:${PORT}`)
+
+app.post('/api/persons', (request, response) => {
+  const newPerson = request.body
+  let messageError = ''
+  if (typeof newPerson.name === 'undefined' || typeof newPerson.number === 'undefined') { messageError = 'The name or number is missing' } else if (newPerson.name.trim() === '') { messageError = 'name is required' } else if (newPerson.number.trim() === '') { messageError = 'number is required' } else {
+    const existsPerson = persons.find(item => item.name === newPerson.name.trim())
+    if (typeof existsPerson !== 'undefined') { messageError = 'name must be unique' }
+  }
+
+  if (messageError === '') {
+    const ids = persons.map(item => item.id)
+    const maxId = Math.max(...ids)
+    newPerson.id = maxId + 1
+    persons = persons.concat(newPerson)
+    response.status(201).json(newPerson)
+  } else { response.status(400).json({ error: messageError }) }
+})
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`listennig by http://localhost:${PORT}`)
 })
